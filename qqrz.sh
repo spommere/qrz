a340e553f9838b6391fee1fa6570631ecf5cf9cb
Stefan Pommerenk <spommere@gmail.com>
2024-10-02 21:55:53 -1000

    error handling

--- qqrz.sh
+++ qqrz.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
   
 # search for a particular posting on QRZ swapmeet page
 #export https_proxy=...
@@ -7,10 +7,28 @@ page=https://qrz.com/page/hotswap.html
 b=`basename $page`
 slp=5m
 
-srch1=V71
-srch2=D710
+srch[1]=V71
+srch[2]=D710
+srch[3]=FT891
 
-tmp=/tmp
+
+for ((i=1;i<=10;i++))
+do
+  if [[ -n "${srch[$i]}" ]]
+  then
+    if [[ $i -eq 1 ]]
+    then
+      srch="${srch[$i]}"
+    else
+      srch="${srch}|${srch[$i]}"
+    fi
+  fi
+done
+
+echo "about to seach for $srch"
+
+tmp=~/qqrz
+mkdir $tmp
 cd $tmp
 
 touch $tmp/qqrz.dat
@@ -19,7 +37,7 @@ do
   rm -f $b
   echo "`/bin/date`: calling wget"
   /bin/wget $page 1>/dev/null 2>&1
-  egrep "$srch1|$srch2" $tmp/$b|grep index.php|grep width > $tmp/qqrz.tmp
+  egrep "$srch" $tmp/$b|grep index.php|grep width > $tmp/qqrz.tmp
   while read line
   do
     link=`echo $line|awk '{print $3}'|cut -f2 -d=`
@@ -29,7 +47,11 @@ do
     then
       echo "$device, $link"
       echo $device >> $tmp/qqrz.dat
-      echo "found $link"|mail -s "QRZ.com: Found $device" -r $email $email
+      which mail
+      if [[ $? -eq 0 ]]
+      then
+        echo "found $link"|mail -s "QRZ.com: Found $device" -r $email $email
+      fi
     fi
   done < $tmp/qqrz.tmp
   echo "`/bin/date`: sleep $slp"
